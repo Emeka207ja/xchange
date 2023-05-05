037ca5b08df7d3b3bf170456c700553b81a22132
imprint <asiwebrightemeka@gmail.com>
2023-05-06 00:33:48 +0100

    fixing the landing page

--- Components/Home/SendeReceive.tsx
+++ Components/Home/SendeReceive.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {
     Box,
     FormLabel,
@@ -12,7 +12,16 @@ import {
 } from '@chakra-ui/react'
 
 const SendeReceive = () => {
-    const [isMobile] = useMediaQuery("(max-width: 768px)"); 
+	const [isMobile] = useMediaQuery("(max-width: 768px)"); 
+	const [coin, setCoin] = useState("Bitcoin")
+	const [receive, setReceive] = useState("naira")
+	
+	const handleChange = (
+		e: React.ChangeEvent<HTMLSelectElement>,
+		fn: React.Dispatch<React.SetStateAction<string>>
+	) => {
+		fn(e.target.value);
+	};
   return (
 		<Box>
 			<form>
@@ -22,18 +31,25 @@ const SendeReceive = () => {
 					justifyContent={"space-around"}
 				>
 					<FormControl>
-						<FormLabel textAlign={"center"}>Send</FormLabel>
+						<FormLabel
+							textAlign={{ md: "center" }}
+							marginLeft={{ base: "1rem", md: "0" }}
+						>
+							Send
+						</FormLabel>
 						<Select
 							placeholder="select"
 							width={{ base: "sm", md: "md" }}
 							display={"relative"}
 							left={{ base: "0", md: "0" }}
+						  value={coin}
+						  onChange={(e)=>handleChange(e,setCoin)}
 						>
-							<option value="option1" style={{ fontSize: "0.6rem" }}>
+							<option value="Bitcoin" style={{ fontSize: "0.6rem" }}>
 								Bitcoin
 							</option>
 							<option
-								value="option2"
+								value="usdt"
 								style={{ fontSize: "0.6rem", width: "4rem" }}
 							>
 								USDT
@@ -48,24 +64,35 @@ const SendeReceive = () => {
 					</FormControl>
 					{!isMobile && (
 						<Box width={"7rem"} pos={"relative"} right={"2rem"} top={"1rem"}>
-							<Image src="/assets/images/exchange.png" alt="" width={"100%"}  objectFit={"cover"}/>
+							<Image
+								src="/assets/images/exchange.png"
+								alt=""
+								width={"100%"}
+								objectFit={"cover"}
+							/>
 						</Box>
 					)}
 					<FormControl>
-						<FormLabel textAlign={"center"}>Receive</FormLabel>
+						<FormLabel
+							textAlign={{ md: "center" }}
+							marginLeft={{ base: "1rem", md: "0" }}
+						>
+							Receive
+						</FormLabel>
 						<Select
 							placeholder="select"
 							width={{ base: "sm", md: "md" }}
 							display={"relative"}
-							left={{ base: "0", md: "0" }}
+						  left={{ base: "0", md: "0" }}
+						  value={receive}
+						  onChange={(e)=>handleChange(e,setReceive)}
 						>
 							<option
-								value="option1"
+								value="naira"
 								style={{ fontSize: "0.6rem", width: "4rem" }}
 							>
 								Naira
 							</option>
-							
 						</Select>
 					</FormControl>
 				</Flex>
