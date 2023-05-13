90c93df323f4b2a99780af8194ef8a47f55a0af9
imprint <asiwebrightemeka@gmail.com>
2023-05-13 18:24:43 +0100

    worked on navigation query strings

--- Components/Home/GiftCardGuildeline.tsx
+++ Components/Home/GiftCardGuildeline.tsx
@@ -9,31 +9,31 @@ interface Props{
 const GiftCardGuildeline = ({name}:Props) => {
   return (
 		<List>
-			<ListItem fontSize={{ base: "0.8rem", md: "1rem" }}>
+			<ListItem fontSize={{ base: "0.7rem", md: "1rem" }}>
 				<ListIcon as={SettingsIcon} color="green.500" />
 				{!name
 					? "Select crypto or the type of fund you want to send"
 					: `tell us how many ${name} you want to exchange for naira`}
 			</ListItem>
-			<ListItem fontSize={{ base: "0.8rem", md: "1rem" }}>
+			<ListItem fontSize={{ base: "0.7rem", md: "1rem" }}>
 				<ListIcon as={SettingsIcon} color="green.500" />
 				Click on exchange button or customer chat to get info to send to
 			</ListItem>
-			<ListItem fontSize={{ base: "0.8rem", md: "1rem" }}>
+			<ListItem fontSize={{ base: "0.7rem", md: "1rem" }}>
 				<ListIcon as={SettingsIcon} color="green.500" />
 				{!name
-					? "State how many crypto or fun you want to send to the info"
-					: `State how many ${name} you want to send to the info`}
+					? "State how many crypto or fund you want to send"
+					: `State how many ${name} you want to send `}
 			</ListItem>
-			<ListItem fontSize={{ base: "0.8rem", md: "1rem" }}>
+			<ListItem fontSize={{ base: "0.7rem", md: "1rem" }}>
 				<ListIcon as={SettingsIcon} color="green.500" />
 				Transfer and wait for confirmation
 			</ListItem>
-			<ListItem fontSize={{ base: "0.8rem", md: "1rem" }}>
+			<ListItem fontSize={{ base: "0.7rem", md: "1rem" }}>
 				<ListIcon as={SettingsIcon} color="green.500" />
 				Confirm reception of funds
 			</ListItem>
-			<ListItem fontSize={{ base: "0.8rem", md: "1rem" }}>
+			<ListItem fontSize={{ base: "0.7rem", md: "1rem" }}>
 				<ListIcon as={SettingsIcon} color="green.500" />
 				Thanks for your patronage
 			</ListItem>
